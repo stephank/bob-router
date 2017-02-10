3a432944c7a517ff369a7798f6503ea6b2315be6
Stéphan Kochen <stephan@kochen.nl>
2017-02-10 11:20:44 +0100

    Child routers.

--- index.js
+++ index.js
@@ -1,7 +1,108 @@
 import Promise from 'bluebird';
 import pathToRegexp from 'path-to-regexp';
 
-export default () => {
+export class Router {
+    constructor() {
+        this.base = '';
+        this.routes = [];
+    }
+
+    // Add a route. Routes are matched in the order they are added.
+    //
+    // Object parameters are defaults to apply to route params when matched.
+    //
+    // String and function parameters are actions to call, which may also
+    // return (a promise for) defaults. They receive the match params object as
+    // payload.
+    add(path, ...steps) {
+        const re = pathToRegexp(path);
+        this.routes.push({ path, steps, re });
+        return this;
+    }
+
+    // Match the path to one of the routes.
+    //
+    // Returns an object with `route` and `params`. If no route is matched,
+    // `route` will be `null`.
+    match(path, params={}) {
+        params.router = this;
+        params.route = this.routes.find(({ re }) => {
+            const match = re.exec(path);
+            if (match) {
+                params.length = 0;
+                re.keys.forEach((key, i) => {
+                    params[key.name] = match[i + 1];
+                    if (typeof key.name === 'number') {
+                        params.length = key.name + 1;
+                    }
+                });
+                return true;
+            }
+        });
+        return params;
+    }
+
+    // Match the path and run route steps.
+    //
+    // Returns an object with `route` and `params`. If no route is matched,
+    // `route` will be `null`, and `params` will have an `error` property.
+    exec({ state, dispatch, commit }, path, params={}) {
+        this.match(path, params);
+        return (params.route ?
+            Promise.mapSeries(params.route.steps, (step) => {
+                switch (typeof step) {
+                    case 'string':
+                        return dispatch(step, params);
+                    case 'function':
+                        return step({ state, dispatch, commit }, params);
+                    default:
+                        Object.assign(params, step);
+                        return;
+                }
+            }) :
+            Promise.reject(Error('Not found'))
+        )
+            .catch((error) => {
+                params.error = error;
+            })
+            .return(params);
+    }
+
+    // Create a new child router, and add a route which delegates to it.
+    //
+    // Any additional arguments are intermediate steps to run, before
+    // delegating to the child router. Returns a new Router instance.
+    child(path, ...steps) {
+        // Strip trailing slashes.
+        let idx = path.length;
+        while (path[--idx] === '/') {};
+        path = path.slice(0, idx + 1);
+        // Build child base path.
+        const base = this.base + path;
+        // Build path we actually match on.
+        path += '/*';
+
+        // Create child router.
+        const RouterClass = this.constructor;
+        const child = new RouterClass();
+        child.base = base;
+        child.parent = this;
+        child.module = this.module;
+
+        // Add route to parent, delegating to child.
+        this.add(path, ...steps, (store, params) => {
+            const restIdx = --params.length;
+            const path = '/' + params[restIdx];
+            delete params[restIdx];
+
+            return child.exec(store, path, params);
+        });
+
+        return child;
+    }
+}
+
+export default (router=new Router()) => {
     // A unique object for every navigation, containing the current path we're
     // navigating to. Used in async navigation to check if preempted.
     let currentHandle = { path: '' };
@@ -12,15 +113,14 @@ export default () => {
         // Current routed state. Params is a combination of route data and
         // path parameters. The data may provide defaults.
         path: '',
-        route: null,
-        params: {}
+        params: {},
     };
 
     const getters = {
         // Get full routing state.
         routing: (state) => state,
         // Get the current route params.
-        routeParams: (state) => state.params
+        routeParams: (state) => state.params,
     };
 
     const mutations = {
@@ -29,12 +129,11 @@ export default () => {
             state.progress = true;
         },
         // Finished navigating.
-        navigated(state, { path, route, params }) {
+        navigated(state, { path, params }) {
             state.progress = false;
             state.path = path;
-            state.route = route;
             state.params = params;
-        }
+        },
     };
 
     const actions = {
@@ -45,92 +144,44 @@ export default () => {
                 return;
             }
             const handle = currentHandle = { path };
-
-            // Resolve route and extract parameters.
-            const params = {};
-            const route = router.routes.find(({ re }) => {
-                const match = re.exec(path);
-                if (match) {
-                    re.keys.forEach((key, i) => {
-                        params[key.name] = match[i + 1];
-                    });
-                    return true;
-                }
-            });
-
-            // Perform all the steps attached to the route.
+            // Match and execute the route.
             commit('navigating');
-            (route ?
-                Promise.mapSeries(route.steps, (step) => {
-                    switch (typeof step) {
-                        case 'string':
-                            return dispatch(step, params);
-                        case 'function':
-                            return step({
-                                state: rootState,
-                                dispatch,
-                                commit
-                            }, params);
-                        default:
-                            Object.assign(params, step);
-                            return;
+            return router.exec({ state: rootState, dispatch, commit}, path)
+                .then((params) => {
+                    // Check if we're still current.
+                    if (currentHandle === handle) {
+                        // Normalize location.hash.
+                        // The change event for this will stop at the guard.
+                        location.hash = '#' + path;
+                        // Commit the change.
+                        commit('navigated', { path, params });
                     }
-                }) :
-                Promise.reject(Error('Not found'))
-            )
-            .catch((error) => {
-                params.error = error;
-            })
-            .then(() => {
-                // Check if we're still current.
-                if (currentHandle === handle) {
-                    // Normalize location.hash.
-                    // The change event for this will stop at the guard.
-                    location.hash = '#' + path;
-
-                    // Commit the change.
-                    commit('navigated', { path, route, params });
-                }
-
-                return params;
-            });
-        }
+                    return params;
+                });
+        },
     };
 
-    // Add a route. Routes are matched in the order they are added.
-    //
-    // Object parameters are defaults to apply to route params when matched.
-    //
-    // String and function parameters are actions to call, which may also
-    // return (a promise for) defaults. They receive the match params object as
-    // payload.
-    const add = (path, ...steps) => {
-        const re = pathToRegexp(path);
-        router.routes.push({ path, steps, re });
-        return router;
-    };
+    // Export a Vuex module.
+    router.module = { state, getters, mutations, actions };
 
     // Update using the current location.hash.
-    const update = ({ dispatch }) => {
+    router.update = ({ dispatch }) => {
         const path = '/' + location.hash.replace(/^[#\/]+/, '');
         return dispatch('navigate', path);
     };
 
-    // Installs the hashchange event listener.
-    const install = ({ dispatch }) => {
+    // Installs the `hashchange` event listener.
+    router.install = ({ dispatch }) => {
         window.addEventListener('hashchange', () => {
-            update({ dispatch });
+            router.update({ dispatch });
         }, false);
     };
 
     // Start routing. Installs the listener and updates once immediately.
-    const start = ({ dispatch }) => {
-        install({ dispatch });
-        return update({ dispatch });
+    router.start = ({ dispatch }) => {
+        router.install({ dispatch });
+        return router.update({ dispatch });
     };
 
-    // Build the module and router objects.
-    const module = { state, getters, mutations, actions };
-    const router = { routes: [], module, add, update, install, start };
     return router;
 };
